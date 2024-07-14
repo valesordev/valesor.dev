a33270af36c7c991b9e022d4a238228d052135b1
Brian Ashburn <ashburn.brian@gmail.com>
2024-07-14 09:39:53 -0700

    Template for gatherer run command

--- app/autobs/internal/gatherer/gatherer.go
+++ app/autobs/internal/gatherer/gatherer.go
@@ -1,7 +1,16 @@
 package gatherer
 
 import (
+	"net/http"
+
 	"github.com/spf13/cobra"
 )
 
-func RunCommand(cmd *cobra.Command, args []string) {}
+func RunCommand(cmd *cobra.Command, args []string) {
+	client := &http.Client{}
+	resp, err := client.Get("http://localhost:8080")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+}
